0e1e4a809aec5d2119006d05ad910ab494452368
karaluv <egr.vrn@gmail.com>
2022-02-01 18:07:34 +0300

    first commit

---
+++ laba1/task1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Воронов Егор Александрович, Б02-112, ЛФИ"<< endl;
+
+system("pause");
+cout << '\n';
+return 0;
+}
---
+++ laba1/task2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int function_power(int x, unsigned int y)
+{
+  if (y == 0) 
+  return 1;
+  if (y == 1) 
+  return x;
+  
+  int rec_y = function_power(x, y/2);
+
+  if (y%2 == 0) 
+  return rec_y * rec_y;
+  else 
+  return x * rec_y * rec_y;
+}
+
+int main()
+{
+int x;
+unsigned int y;
+
+cin >> x;
+cin >> y;
+cout << function_power(x,y) << '\n';
+system("pause");
+cout << '\n';
+return 0;
+}
